5e9904992506002919176a5fe6ff746e7843216c
wuchanghua <1453346832@qq.com>
2023-07-14 21:48:14 +0800

    基本的结构，试一下git的配置是不是对的，没有任何东西，回退到这里没有意义

---
+++ resume/src/views/Wu-Resume.vue
@@ -0,0 +1,17 @@
+<template>
+1
+</template>
+
+<script>
+  export default {
+    name: 'wuResume',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
